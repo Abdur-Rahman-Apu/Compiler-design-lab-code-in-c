caf62ed21a92f102117c3118b3002e76caf877e8
Abdur Rahman Apu <60545278+Abdur-Rahman-Apu@users.noreply.github.com>
2022-01-31 21:14:15 +0600

    Add files via upload

---
+++ Recognized a* and a*b+/valid_invalid.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char str[50];
+    gets(str);
+    int len=strlen(str);
+
+    if(under_a_star(str,len,0,0)||under_a_star_plus_b_positive(str,len,0,0)){
+        printf("Recognized");
+    }else{
+        printf("Not recognized");
+    }
+
+    return 0;
+}
+int under_a_star(char str[50],int len,int state,int i){
+    for(i=0;i<len;i++){
+        if(state==0 && str[i]==' '){
+            state=0;
+        }else if(state==0 && str[i]=='a'){
+            state=0;
+        }else{
+            return 0;
+        }
+    }
+    if(state==0){
+        return 1;
+    }
+}
+
+int under_a_star_plus_b_positive(char str[50],int len,int state,int i){
+
+    for(i=0;i<len;i++){
+        if(state==0&&str[i]=='a'){
+            state=0;
+        }else if((state==0||state==1)&&str[i]=='b'){
+            state=1;
+        }else if(state==1 && str[i]!='b'){
+            return 0;
+        }else{
+            return 0;
+        }
+    }
+    if(state==1){
+        return 1;
+    }else{
+        return 0;
+    }
+
+}
